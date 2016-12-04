9c9a1a8d797692409f766cfabd8cf3be23d07020
Capillary J <jason9075@gmail.com>
2016-12-04 08:42:22 +0800

    學生版新增圖片

--- src/main/java/com/jason9075/womanyhackathon/MainActivity.java
+++ src/main/java/com/jason9075/womanyhackathon/MainActivity.java
@@ -48,6 +48,23 @@ public class MainActivity extends AppCompatActivity {
     private TextView userTextView;
     private ImageButton internetStatusButton;
 
+    private double[] latArray = new double[]{
+            25.0354104, 25.0355104, 25.0356104, 25.0357104, 25.0358104,
+            25.0359104, 25.0360104, 25.0361104, 25.0362104, 25.0363104,
+            25.0364104, 25.0365104, 25.0366104, 25.0367104, 25.0368104,
+            25.0369104, 25.0370104, 25.0371104, 25.0372104, 25.0373104,
+            25.0374104, 25.0375104, 25.0376104, 25.0377104, 25.0378104,
+            25.0367904, 25.0380104, 25.0381104, 25.0382104, 25.0383104};
+    private double[] lngArray = new double[]{
+            121.5695895, 121.5696895, 121.5697895, 121.5698895, 121.5699995,
+            121.5670095, 121.5671895, 121.5672895, 121.5673895, 121.5674895,
+            121.5675895, 121.5676895, 121.5677895, 121.5678895, 121.5679895,
+            121.5680895, 121.5681895, 121.5682895, 121.5683895, 121.5684895,
+            121.5685895, 121.5686895, 121.5687895, 121.5688895, 121.5689895,
+            121.5690895, 121.5691895, 121.5692895, 121.5693895, 121.5694895};
+
+    private int currentIndex = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         compositeSubscription.add(Observable.just(1)
-                .delay(10, TimeUnit.SECONDS)
+                .delay(2, TimeUnit.SECONDS)
                 .observeOn(AndroidSchedulers.mainThread())
                 .flatMap(new Func1<Object, Observable<GoogleMapLocationResult>>() {
                     @Override
@@ -102,12 +119,19 @@ public class MainActivity extends AppCompatActivity {
                         if (currentLocation != null && !pref.getUserName().equals("")) {
                             System.out.println(">>>送出一筆資料");
                             StudentLocationData studentLocationData = new StudentLocationData(UUID.randomUUID().toString(), pref.getUserName());
-                            studentLocationData.setLatitude(currentLocation.getLatitude());
-                            studentLocationData.setLongitude(currentLocation.getLongitude());
+                            if (currentIndex < latArray.length) {
+                                studentLocationData.setLatitude(latArray[currentIndex]);
+                                studentLocationData.setLongitude(lngArray[currentIndex]);
+                            } else {
+                                studentLocationData.setLatitude(studentLocationData.getLatitude());
+                                studentLocationData.setLongitude(studentLocationData.getLongitude());
+                            }
+
                             studentLocationData.setAddress(RetrofitManager.INSTANCE.getLastAddress());
                             SimpleDateFormat formatter = DateFormatCached.INSTANCE.getFormat("yyyy/MM/dd HH:mm:ss");
                             studentLocationData.setDate(formatter.format(new Date()));
                             mFirebaseDatabaseReference.child(STUDENT_LOCATION_TABLE).push().setValue(studentLocationData);
+                            currentIndex++;
                         }
 
                     }
